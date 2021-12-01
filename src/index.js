204cf5d1b7b9ddf6cc293227a6854668fa8cfb2c
Felipe Martins de Jesus <felipe.jesus@basis.com.br>
2021-12-01 00:09:07 -0300

    checksCreateTodosUserAvailability

--- src/index.js
+++ src/index.js
@@ -24,7 +24,13 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Limited add todos of plan trail!' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
